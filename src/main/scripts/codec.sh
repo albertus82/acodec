24f2fdf32ce47d4b35024fd9e3516df31a3fe905
Albertus82 <albertus82@gmail.com>
2016-02-21 13:59:21 +0100

    Fixed bash script.

--- src/main/scripts/codec.sh
+++ src/main/scripts/codec.sh
@@ -1,11 +1,11 @@
 if [ "$1" = "" ]
   then if [ "$JAVA_HOME" != "" ]
-  then "$JAVA_HOME/bin/java" -Xms4m -Xmx8m -jar codec.jar
-  else java -Xms4m -Xmx8m -jar codec.jar $1 $2 $3
+  then "$JAVA_HOME/bin/java" -Xms8m -Xmx32m -jar codec.jar
+  else java -Xms8m -Xmx32m -jar codec.jar
   fi
 else
   if [ "$JAVA_HOME" != "" ]
-  then "$JAVA_HOME/bin/java" -Xms8m -Xmx32m -jar codec.jar
-  else java -Xms8m -Xmx32m -jar codec.jar
+  then "$JAVA_HOME/bin/java" -Xms4m -Xmx8m -jar codec.jar $1 $2 $3
+  else java -Xms4m -Xmx8m -jar codec.jar $1 $2 $3
   fi
 fi
