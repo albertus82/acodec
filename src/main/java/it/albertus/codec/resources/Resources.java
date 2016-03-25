f2be33adce8810acc1f19960edf57583dc29b56a
Albertus82 <albertus82@gmail.com>
2016-03-25 12:24:03 +0100

    Refactoring.

--- src/main/java/it/albertus/codec/resources/Resources.java
+++ src/main/java/it/albertus/codec/resources/Resources.java
@@ -21,7 +21,7 @@ public class Resources {
 
 	public static String get(final String key, final Object... params) {
 		final List<String> stringParams = new ArrayList<String>(params.length);
-		for (Object param : params) {
+		for (final Object param : params) {
 			stringParams.add(param != null ? param.toString() : "");
 		}
 		final String message = MessageFormat.format(resources.getString(key), stringParams.toArray());
