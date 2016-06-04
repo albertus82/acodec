c47580f985cf2cb25ea10065309021e5f837ba88
Albertus82 <albertus82@gmail.com>
2016-06-04 14:42:12 +0200

    Linux shell script (classpath).

--- src/main/scripts/codec.sh
+++ src/main/scripts/codec.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 if [ "$1" = "" ]
   then if [ "$JAVA_HOME" != "" ]
-  then "$JAVA_HOME/bin/java" -Xms4m -Xmx32m -jar codec.jar
-  else java -Xms4m -Xmx32m -jar codec.jar
+  then "$JAVA_HOME/bin/java" -Xms4m -Xmx32m -classpath "codec.jar:lib/*" it.albertus.codec.Codec
+  else java -Xms4m -Xmx32m -classpath "codec.jar:lib/*" it.albertus.codec.Codec
   fi
 else
   if [ "$JAVA_HOME" != "" ]
-  then "$JAVA_HOME/bin/java" -Xms4m -Xmx8m -jar codec.jar $1 $2 $3 $4 $5 $6
-  else java -Xms4m -Xmx8m -jar codec.jar $1 $2 $3 $4 $5 $6
+  then "$JAVA_HOME/bin/java" -Xms4m -Xmx8m -classpath "codec.jar:lib/*" it.albertus.codec.Codec $1 $2 $3 $4 $5 $6
+  else java -Xms4m -Xmx8m -classpath "codec.jar:lib/*" it.albertus.codec.Codec $1 $2 $3 $4 $5 $6
   fi
 fi
