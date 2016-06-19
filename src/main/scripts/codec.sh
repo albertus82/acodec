7deb928db707ec6037f8f8abfa69897d0ce9a4e2
Albertus82 <albertus82@gmail.com>
2016-06-19 21:21:52 +0200

    Improved shell script.

--- src/main/scripts/codec.sh
+++ src/main/scripts/codec.sh
@@ -1,12 +1,23 @@
 #!/bin/sh
+PRG="$0"
+while [ -h "$PRG" ]; do
+  ls=`ls -ld "$PRG"`
+  link=`expr "$ls" : '.*-> \(.*\)$'`
+  if expr "$link" : '/.*' > /dev/null; then
+    PRG="$link"
+  else
+    PRG=`dirname "$PRG"`/"$link"
+  fi
+done
+PRGDIR=`dirname "$PRG"`
 if [ "$1" = "" ]
   then if [ "$JAVA_HOME" != "" ]
-  then "$JAVA_HOME/bin/java" -Xms4m -Xmx32m -classpath "codec.jar:lib/*" it.albertus.codec.Codec
-  else java -Xms4m -Xmx32m -classpath "codec.jar:lib/*" it.albertus.codec.Codec
+  then "$JAVA_HOME/bin/java" -Xms4m -Xmx32m -classpath "$PRGDIR/codec.jar:$PRGDIR/lib/*" it.albertus.codec.Codec
+  else java -Xms4m -Xmx32m -classpath "$PRGDIR/codec.jar:$PRGDIR/lib/*" it.albertus.codec.Codec
   fi
 else
   if [ "$JAVA_HOME" != "" ]
-  then "$JAVA_HOME/bin/java" -Xms4m -Xmx8m -classpath "codec.jar:lib/*" it.albertus.codec.Codec $1 $2 $3 $4 $5 $6
-  else java -Xms4m -Xmx8m -classpath "codec.jar:lib/*" it.albertus.codec.Codec $1 $2 $3 $4 $5 $6
+  then "$JAVA_HOME/bin/java" -Xms4m -Xmx8m -classpath "$PRGDIR/codec.jar:$PRGDIR/lib/*" it.albertus.codec.Codec $1 $2 $3 $4 $5 $6
+  else java -Xms4m -Xmx8m -classpath "$PRGDIR/codec.jar:$PRGDIR/lib/*" it.albertus.codec.Codec $1 $2 $3 $4 $5 $6
   fi
 fi
