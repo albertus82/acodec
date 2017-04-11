ea9c7c3db60f35d25b9ec47047b3592f566c4bbf
Alberto <albertus82@gmail.com>
2017-04-11 09:03:30 +0200

    Some Java 7 fixes.

--- src/main/java/it/albertus/codec/engine/Ascii85.java
+++ src/main/java/it/albertus/codec/engine/Ascii85.java
@@ -16,37 +16,17 @@ public class Ascii85 {
 	}
 
 	public static String encode(final byte[] byteArray) throws IOException {
-		ByteArrayInputStream bais = null;
-		ByteArrayOutputStream baos = null;
-		ASCII85OutputStream a85os = null;
-		try {
-			bais = new ByteArrayInputStream(byteArray);
-			baos = new ByteArrayOutputStream();
-			a85os = new ASCII85OutputStream(baos);
+		final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		try (final ByteArrayInputStream bais = new ByteArrayInputStream(byteArray); final ASCII85OutputStream a85os = new ASCII85OutputStream(baos)) {
 			IOUtils.copy(bais, a85os);
-		}
-		finally {
-			IOUtils.closeQuietly(a85os);
-			IOUtils.closeQuietly(baos);
-			IOUtils.closeQuietly(bais);
 		}
 		return baos.toString().replaceAll("[" + NewLine.CRLF.toString() + "]+", "");
 	}
 
 	public static byte[] decode(final String encoded) throws IOException {
-		ByteArrayInputStream bais = null;
-		Ascii85InputStream a85is = null;
-		ByteArrayOutputStream baos = null;
-		try {
-			bais = new ByteArrayInputStream(encoded.getBytes());
-			a85is = new Ascii85InputStream(bais);
-			baos = new ByteArrayOutputStream();
+		final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		try (final ByteArrayInputStream bais = new ByteArrayInputStream(encoded.getBytes()); final Ascii85InputStream a85is = new Ascii85InputStream(bais)) {
 			IOUtils.copy(a85is, baos);
-		}
-		finally {
-			IOUtils.closeQuietly(baos);
-			IOUtils.closeQuietly(a85is);
-			IOUtils.closeQuietly(bais);
 		}
 		return baos.toByteArray();
 	}
--- src/main/java/it/albertus/codec/engine/Base91.java
+++ src/main/java/it/albertus/codec/engine/Base91.java
@@ -4,8 +4,6 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
-import org.apache.commons.io.IOUtils;
-
 import net.sourceforge.base91.b91cli;
 
 public class Base91 {
@@ -15,31 +13,17 @@ public class Base91 {
 	}
 
 	public static String encode(final byte[] byteArray) throws IOException {
-		ByteArrayInputStream bais = null;
-		ByteArrayOutputStream baos = null;
-		try {
-			bais = new ByteArrayInputStream(byteArray);
-			baos = new ByteArrayOutputStream();
+		final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		try (final ByteArrayInputStream bais = new ByteArrayInputStream(byteArray)) {
 			b91cli.encode(bais, baos);
-		}
-		finally {
-			IOUtils.closeQuietly(baos);
-			IOUtils.closeQuietly(bais);
 		}
 		return baos.toString().trim();
 	}
 
 	public static byte[] decode(final String encoded) throws IOException {
-		ByteArrayInputStream bais = null;
-		ByteArrayOutputStream baos = null;
-		try {
-			bais = new ByteArrayInputStream(encoded.getBytes());
-			baos = new ByteArrayOutputStream();
+		final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		try (final ByteArrayInputStream bais = new ByteArrayInputStream(encoded.getBytes())) {
 			b91cli.decode(bais, baos);
-		}
-		finally {
-			IOUtils.closeQuietly(baos);
-			IOUtils.closeQuietly(bais);
 		}
 		return baos.toByteArray();
 	}
