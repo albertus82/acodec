367af16eee671b2f4f0f4b4512b09f8ac8f6a4eb
Alberto <albertus82@gmail.com>
2017-08-30 22:29:30 +0200

    Fixed shell scripts.

--- src/main/scripts/codec.sh
+++ src/main/scripts/codec.sh
@@ -12,12 +12,12 @@ done
 PRGDIR=`dirname "$PRG"`
 if [ "$1" = "" ]
   then if [ "$JAVA_HOME" != "" ]
-  then "$JAVA_HOME/bin/java" -DSWT_GTK3=0 -Xms4m -Xmx32m -classpath "$PRGDIR/codec.jar:$PRGDIR/lib/*" it.albertus.codec.Codec
-  else java -DSWT_GTK3=0 -Xms4m -Xmx32m -classpath "$PRGDIR/codec.jar:$PRGDIR/lib/*" it.albertus.codec.Codec
+  then "$JAVA_HOME/bin/java" -DSWT_GTK3=0 -Xms8m -Xmx32m -classpath "$PRGDIR/codec.jar:$PRGDIR/lib/*" it.albertus.codec.Codec
+  else java -DSWT_GTK3=0 -Xms8m -Xmx32m -classpath "$PRGDIR/codec.jar:$PRGDIR/lib/*" it.albertus.codec.Codec
   fi
 else
   if [ "$JAVA_HOME" != "" ]
-  then "$JAVA_HOME/bin/java" -Xms4m -Xmx8m -classpath "$PRGDIR/codec.jar:$PRGDIR/lib/*" it.albertus.codec.Codec $1 $2 $3 $4 $5 $6
-  else java -Xms4m -Xmx8m -classpath "$PRGDIR/codec.jar:$PRGDIR/lib/*" it.albertus.codec.Codec $1 $2 $3 $4 $5 $6
+  then "$JAVA_HOME/bin/java" -Xms4m -Xmx16m -classpath "$PRGDIR/codec.jar:$PRGDIR/lib/*" it.albertus.codec.Codec $1 $2 $3 $4 $5 $6
+  else java -Xms4m -Xmx16m -classpath "$PRGDIR/codec.jar:$PRGDIR/lib/*" it.albertus.codec.Codec $1 $2 $3 $4 $5 $6
   fi
 fi
