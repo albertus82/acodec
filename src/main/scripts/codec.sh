50905c9b4b54ae2d5d5e0aff847387fb6ee662e4
Alberto <albertus82@users.noreply.github.com>
2018-12-21 21:27:18 +0100

    New build.

--- src/main/scripts/codec.sh
+++ src/main/scripts/codec.sh
@@ -12,12 +12,12 @@ done
 PRGDIR=`dirname "$PRG"`
 if [ "$1" = "" ]
   then if [ "$JAVA_HOME" != "" ]
-  then "$JAVA_HOME/bin/java" -DSWT_GTK3=0 -Xms8m -Xmx32m -classpath "$PRGDIR/codec.jar:$PRGDIR/lib/*" it.albertus.codec.Codec
-  else java -DSWT_GTK3=0 -Xms8m -Xmx32m -classpath "$PRGDIR/codec.jar:$PRGDIR/lib/*" it.albertus.codec.Codec
+  then "$JAVA_HOME/bin/java" -DSWT_GTK3=0 -Xms@vm.initialHeapSize@m -Xmx@vm.maxHeapSize@m -classpath "$PRGDIR/@artifactId@.jar:$PRGDIR/lib/*" @mainClass@
+  else java -DSWT_GTK3=0 -Xms@vm.initialHeapSize@m -Xmx@vm.maxHeapSize@m -classpath "$PRGDIR/@artifactId@.jar:$PRGDIR/lib/*" @mainClass@
   fi
 else
   if [ "$JAVA_HOME" != "" ]
-  then "$JAVA_HOME/bin/java" -Xms4m -Xmx16m -classpath "$PRGDIR/codec.jar:$PRGDIR/lib/*" it.albertus.codec.Codec $1 $2 $3 $4 $5 $6
-  else java -Xms4m -Xmx16m -classpath "$PRGDIR/codec.jar:$PRGDIR/lib/*" it.albertus.codec.Codec $1 $2 $3 $4 $5 $6
+  then "$JAVA_HOME/bin/java" -Xms@console.vm.initialHeapSize@m -Xmx@console.vm.maxHeapSize@m -classpath "$PRGDIR/@artifactId@.jar:$PRGDIR/lib/*" @mainClass@ $1 $2 $3 $4 $5 $6
+  else java -Xms@console.vm.initialHeapSize@m -Xmx@console.vm.maxHeapSize@m -classpath "$PRGDIR/@artifactId@.jar:$PRGDIR/lib/*" @mainClass@ $1 $2 $3 $4 $5 $6
   fi
 fi
