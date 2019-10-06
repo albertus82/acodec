9d7f104bb43bae663057480881b59147357052ae
Alberto <albertus82@users.noreply.github.com>
2019-10-06 18:35:37 +0200

    Project update.

--- src/main/scripts/Codec.bat
+++ src/main/scripts/Codec.bat
@@ -1,11 +1,11 @@
 @ECHO OFF
 IF "%1" == "" GOTO GUI
 :CON
-IF "%JAVA_HOME%" == "" java.exe -Xms@console.vm.initialHeapSize@m -Xmx@console.vm.maxHeapSize@m -jar "%~dp0@launch4j.jarName@" %1 %2 %3 %4 %5 %6
-IF NOT "%JAVA_HOME%" == "" "%JAVA_HOME%\bin\java.exe" -Xms@console.vm.initialHeapSize@m -Xmx@console.vm.maxHeapSize@m -jar "%~dp0@launch4j.jarName@" %1 %2 %3 %4 %5 %6
+IF "%JAVA_HOME%" == "" java.exe -Xms@console.vm.initialHeapSize@m -Xmx@console.vm.maxHeapSize@m -jar "%~dp0@windows.jarFileName@" %1 %2 %3 %4 %5 %6
+IF NOT "%JAVA_HOME%" == "" "%JAVA_HOME%\bin\java.exe" -Xms@console.vm.initialHeapSize@m -Xmx@console.vm.maxHeapSize@m -jar "%~dp0@windows.jarFileName@" %1 %2 %3 %4 %5 %6
 GOTO END
 :GUI
-IF "%JAVA_HOME%" == "" START "" javaw.exe -Xms@vm.initialHeapSize@m -Xmx@vm.maxHeapSize@m -jar "%~dp0@launch4j.jarName@"
-IF NOT "%JAVA_HOME%" == "" START "" "%JAVA_HOME%\bin\javaw.exe" -Xms@vm.initialHeapSize@m -Xmx@vm.maxHeapSize@m -jar "%~dp0@launch4j.jarName@"
+IF "%JAVA_HOME%" == "" START "" javaw.exe -Xms@vm.initialHeapSize@m -Xmx@vm.maxHeapSize@m -jar "%~dp0@windows.jarFileName@"
+IF NOT "%JAVA_HOME%" == "" START "" "%JAVA_HOME%\bin\javaw.exe" -Xms@vm.initialHeapSize@m -Xmx@vm.maxHeapSize@m -jar "%~dp0@windows.jarFileName@"
 GOTO END
 :END
