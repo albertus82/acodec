65892df97ae9ab9e65ace952de558fa16739e241
Alberto <albertus82@users.noreply.github.com>
2021-02-19 09:16:29 +0100

    TextKeyListener2

---
+++ src/main/java/it/albertus/acodec/gui/listener/TextKeyListener2.java
@@ -0,0 +1,30 @@
+package it.albertus.acodec.gui.listener;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.dnd.Clipboard;
+import org.eclipse.swt.dnd.TextTransfer;
+import org.eclipse.swt.dnd.Transfer;
+import org.eclipse.swt.events.KeyAdapter;
+import org.eclipse.swt.events.KeyEvent;
+import org.eclipse.swt.widgets.Text;
+
+import it.albertus.jface.SwtUtils;
+import it.albertus.jface.closeable.CloseableClipboard;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class TextKeyListener2 extends KeyAdapter {
+
+	private final Text text;
+
+	@Override
+	public void keyPressed(final KeyEvent event) {
+		// Supporto CTRL+C per "Copia"...
+		if (event.stateMask == SWT.MOD1 && event.keyCode == SwtUtils.KEY_COPY) {
+			try (final CloseableClipboard cc = new CloseableClipboard(new Clipboard(text.getDisplay()))) {
+				cc.getClipboard().setContents(new String[] { text.getText() }, new Transfer[] { TextTransfer.getInstance() });
+			}
+		}
+	}
+
+}
