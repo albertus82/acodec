dd137f1c1a7d7e55593de2914cd1b2293289df6a
Alberto <albertus82@users.noreply.github.com>
2021-02-25 21:36:32 +0100

    Improved --version output.

--- src/main/java/it/albertus/acodec/console/VersionProvider.java
+++ src/main/java/it/albertus/acodec/console/VersionProvider.java
@@ -1,5 +1,8 @@
 package it.albertus.acodec.console;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+
 import it.albertus.acodec.console.resources.ConsoleMessages;
 import it.albertus.util.Version;
 import picocli.CommandLine.IVersionProvider;
@@ -9,8 +12,8 @@ public class VersionProvider implements IVersionProvider {
 	private static final ConsoleMessages messages = ConsoleMessages.INSTANCE;
 
 	@Override
-	public String[] getVersion() {
-		return new String[] { messages.get("console.version", Version.getNumber()) };
+	public String[] getVersion() throws ParseException {
+		return new String[] { messages.get("console.version", Version.getNumber(), DateFormat.getDateInstance(DateFormat.MEDIUM).format(Version.getDate())) };
 	}
 
 }
