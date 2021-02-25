b9755959c8703ec9f24415c94703c079ec36da42
Alberto <albertus82@users.noreply.github.com>
2021-02-25 21:59:05 +0100

    Refactoring.

--- src/main/java/it/albertus/acodec/console/resources/ConsoleMessages.java
+++ src/main/java/it/albertus/acodec/console/resources/ConsoleMessages.java
@@ -3,7 +3,6 @@ package it.albertus.acodec.console.resources;
 import java.util.ResourceBundle;
 
 import it.albertus.acodec.common.resources.CommonMessages;
-import it.albertus.acodec.common.resources.ConfigurableMessages;
 import it.albertus.acodec.common.resources.Messages;
 import lombok.NonNull;
 
@@ -11,7 +10,7 @@ public enum ConsoleMessages implements Messages {
 
 	INSTANCE;
 
-	private static final ConfigurableMessages commonMessages = CommonMessages.INSTANCE;
+	private static final Messages commonMessages = CommonMessages.INSTANCE;
 
 	private ResourceBundle resourceBundle = ResourceBundle.getBundle(getClass().getName().toLowerCase(), ResourceBundle.Control.getNoFallbackControl(ResourceBundle.Control.FORMAT_PROPERTIES));
 
