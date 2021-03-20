616a4172a06147a2631024ff92bc06edddd6b25d
Alberto <albertus82@users.noreply.github.com>
2021-03-20 23:37:31 +0100

    fix

--- lib/refresh.sh
+++ lib/refresh.sh
@@ -18,4 +18,4 @@ mvn verify -V -f "$PRGDIR/pom.xml" -P gtk-linux-x86
 mvn verify -V -f "$PRGDIR/pom.xml" -P gtk-linux-x86_64
 mvn verify -V -f "$PRGDIR/pom.xml" -P win32-win32-x86
 mvn verify -V -f "$PRGDIR/pom.xml" -P win32-win32-x86_64
-tree /f "$PRGDIR/."
+tree "$PRGDIR/."
