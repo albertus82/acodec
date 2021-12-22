d4de75cb61dd79c0b509b5cede716664b634b73e
Alberto <albertus82@users.noreply.github.com>
2021-12-22 20:12:17 +0100

    Fix #9

--- src/main/java/io/github/albertus82/acodec/gui/listener/AlgorithmComboSelectionListener.java
+++ src/main/java/io/github/albertus82/acodec/gui/listener/AlgorithmComboSelectionListener.java
@@ -24,34 +24,37 @@ public class AlgorithmComboSelectionListener extends SelectionAdapter {
 
 	@Override
 	public void widgetSelected(final SelectionEvent event) {
-		final CodecAlgorithm algorithm = CodecAlgorithm.values()[gui.getAlgorithmCombo().getSelectionIndex()];
-		gui.setAlgorithm(algorithm);
+		final int selectionIndex = gui.getAlgorithmCombo().getSelectionIndex();
+		if (selectionIndex != -1) {
+			final CodecAlgorithm algorithm = CodecAlgorithm.values()[selectionIndex];
+			gui.setAlgorithm(algorithm);
 
-		/* Enable drag and drop & Process file button */
-		if (!gui.getProcessFileButton().getEnabled()) {
-			gui.getProcessFileButton().setEnabled(true);
-			gui.getShellDropTarget().setTransfer(new Transfer[] { FileTransfer.getInstance(), TextTransfer.getInstance() }); // NOSONAR SWT v4.3.2 does not have the vararg.
-		}
-
-		/* Radios management */
-		final Button encodeRadio = gui.getModeRadios().get(ENCODE);
-		final Button decodeRadio = gui.getModeRadios().get(DECODE);
-
-		if (algorithm.getModes().contains(DECODE)) {
-			if (!decodeRadio.getEnabled()) {
-				decodeRadio.setEnabled(true);
+			/* Enable drag and drop & Process file button */
+			if (!gui.getProcessFileButton().getEnabled()) {
+				gui.getProcessFileButton().setEnabled(true);
+				gui.getShellDropTarget().setTransfer(new Transfer[] { FileTransfer.getInstance(), TextTransfer.getInstance() }); // NOSONAR SWT v4.3.2 does not have the vararg.
 			}
-		}
-		else {
-			if (decodeRadio.getEnabled()) {
-				decodeRadio.setSelection(false);
-				decodeRadio.setEnabled(false);
-				encodeRadio.setSelection(true);
-				encodeRadio.notifyListeners(SWT.Selection, null);
-			}
-		}
 
-		gui.evaluateInputText();
+			/* Radios management */
+			final Button encodeRadio = gui.getModeRadios().get(ENCODE);
+			final Button decodeRadio = gui.getModeRadios().get(DECODE);
+
+			if (algorithm.getModes().contains(DECODE)) {
+				if (!decodeRadio.getEnabled()) {
+					decodeRadio.setEnabled(true);
+				}
+			}
+			else {
+				if (decodeRadio.getEnabled()) {
+					decodeRadio.setSelection(false);
+					decodeRadio.setEnabled(false);
+					encodeRadio.setSelection(true);
+					encodeRadio.notifyListeners(SWT.Selection, null);
+				}
+			}
+
+			gui.evaluateInputText();
+		}
 	}
 
 }
